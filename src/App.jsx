85da01f3f08e56fa227afea9d3ff7fae661de657
newton-w <71337527+newton-w@users.noreply.github.com>
2023-02-13 18:27:52 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-    <div className="duration-300 bg-VeryLightGray dark:bg-DVeryDarkBlue w-full min-h-screen lg:h-full">
+    <main>
+      <div className="duration-300 bg-VeryLightGray dark:bg-DVeryDarkBlue w-full min-h-screen lg:h-full">
+        <Nav />
+        <Router>
+          <Routes>
+            <Route path="/" element={<Countries />} />
+            <Route path="/results" element={<Results />} />
+            <Route path="/results/bordercountry" element={<Bordercountry />} />
+          </Routes>
+        </Router>
 
-      <Nav />
-
-      <Router>
-        <Routes>
-          <Route path="/" element={<Countries />} />
-          <Route path="/results" element={<Results />} />
-          <Route path="/results/bordercountry" element={<Bordercountry />} />
-        </Routes>
-      </Router>
-
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </main>
   )
 }
 
--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,4 @@
-import React, { useState,  useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import Input from './Input'
 import { Link } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid';
@@ -6,10 +6,10 @@ const Countries = () => {
     const [input, setInput] = useState(null)
     const [data, setData] = useState()
 
-    
+
 
     return (
-        <main>
+        <>
             <Input input={input} setInput={setInput} data={data} setData={setData} />
             <div className='flex flex-col justify-center items-center'>
                 <div className='container mx-auto flex flex-col items-center'>
@@ -18,10 +18,10 @@ const Countries = () => {
                         {
                             data && data.map((item) => {
                                 return (
-                                    <Link to= "/results" state={item} >
+                                    <Link to="/results" state={item} >
                                         <div className=' bg-VeryLightGray dark:bg-DVeryDarkBlue text-LVeryDarkBlue dark:text-white rounded-lg w-[20.375rem] h-[26.063rem]  shadow-lg lg:h-auto lg:w-[16.5rem] lg:pb-3 cursor-pointer' key={item.cca3}>
                                             <div>
-                                                <img src={item.flags.png} alt="" className=' w-full h-[12.5rem] object-cover lg:[9.875rem]' loading='lazy' />
+                                                <img src={item.flags.png} alt="" className=' w-full h-[12.5rem] object-cover lg:[9.875rem]' />
                                             </div>
                                             <div className=' px-8'>
                                                 <h1 className=' py-9 font-Nunito font-bold text-lg lg:py-[2.063rem]'>{item.name.common}</h1>
@@ -39,7 +39,7 @@ const Countries = () => {
                     </div>
                 </div>
             </div>
-        </main>
+        </>
     )
 }
 
